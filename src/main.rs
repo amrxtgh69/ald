7d14334b7f09b38a4b435ef4d312423464a79565
amrxtgh <ghimireamrit619@gmail.com>
2026-04-24 20:22:10 +0545

    added the basic struct for this

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use std::process::Command;
 
+//////////////  
+// ~amrxtgh //
+//////////////
+
+enum SymbolBind { LOCAL, GLOBAL, WEAK }
+struct Symbol {
+    // core identification
+    name: String,
+    index: u32,
+    // address/value (changes during linking)
+    value: u64,
+
+    // Symbol characteristics
+    bind: SymbolBind,
+}
+
 fn main() {
     Command::new("gcc")
         .args(&["-c", "tests/test_add.c", "-o", "tests/test_add.o"])
